88599dc22f3899033f40da1985f35b26fba5e422
Samson kiran <82866972+samsonkiran02@users.noreply.github.com>
2021-04-22 15:54:20 +0530

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,1 +1,12 @@
+/*for offline and online*/
+window.addEventListener("online", onFunction);
+window.addEventListener("offline", offFunction);
+
+function onFunction() {
+  alert ("Your browser is working online.");
+}
+
+function offFunction() {
+  alert ("Your browser is working offline.");
+}
 
